ddd5cc5d8feaa8a1de304939d4b36248665efbc4
Andrés Poveda <81783473+apovedq@users.noreply.github.com>
2024-10-23 12:54:36 -0500

    finished project

--- index.js
+++ index.js
@@ -1,30 +1,105 @@
 import express from 'express';
-import { PORT } from './config.js';
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser';
+
+//Imports
+import { PORT, SECRET_JWT_KEY } from './config.js';
 import { UserRepository } from './user-repository.js';
 
+
 const app = express();
+
 // Using Middleware to parse request body to JSON
 app.use(express.json());
 
+//Using middleware to parse the cookies
+app.use(cookieParser());
+
+//Create middleware to indentify if petition contains a cookie
+app.use((req, res, next) => {
+
+    const token = req.cookies.access_token
+
+
+    //Initally start the session as null
+    req.session = { user: null }
+
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY)
+        req.session.user = data
+    } catch { }
+
+    next() //Continue with the route of middlewares
+})
+
+//Using view engine ejs for html responses
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index')
 })
 
 app.post('/register', async (req, res) => {
 
     //1. Recover username and password
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
     try {
         //2. Store and send user id
-        const id = await UserRepository.create({username, password});
-    
-        res.send({id})
+        const id = await UserRepository.create({ username, password });
+
+        res.send({ id })
     } catch (error) {
         //responde with 400 status and the error message
-   
-        res.status(400).send({error: error.message});
+
+        res.status(400).send({ error: error.message });
     }
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const user = await UserRepository.login({ username, password })
+
+        console.log(user)
+
+        //Create the JSON session token
+        const token = jwt.sign(
+            { id: user._id, username: user.username },
+            SECRET_JWT_KEY,
+            {
+                expiresIn: '1h'
+            })
+
+        //Emit response with cookie and then actual response
+        res
+            .cookie('access_token', token, {
+                httpOnly: true, //Cookie is only accesibale in the server
+                secure: process.env.NODE_ENV === 'production', //Cookie is only accessible in https
+                sameSite: 'strict', //Only accessible from the same domain
+                maxAge: 1000 * 60 * 60 // Cookie only lasts for one hour
+            })
+            .send({ user })
+    } catch (error) {
+        res.status(401).send({ error: error.message })
+    }
+
+})
+
+app.get('/protected', (req, res) => {
+
+    //Recover access token from requesst
+    const { user } = req.session
+    if (!user) return res.status(403).send('Access not authorized')
+
+    res.render('protected', user)
+
+})
+
+app.post('/logout', (req, res) => {
+    res
+    .clearCookie('acess_token')
+    .json({message: 'Logout sucessful'})
 })
 
 app.listen(PORT, () => {
--- user-repository.js
+++ user-repository.js
@@ -1,43 +1,32 @@
 import DBLocal from "db-local";
-const {Schema} = new DBLocal({path: './db'});
+const { Schema } = new DBLocal({ path: './db' });
 
 //Using crypto to hash the password before storing it in the db
 import bcrypt from 'bcrypt';
 
 //Salt round refers the to the amount of times the item to encrypt will be encrypted
-import {SALT_ROUNDS} from './config.js'
+import { SALT_ROUNDS } from './config.js'
 
 const User = Schema('User', {
-    _id: {type:String, required:true},
-    username: {type:String, required:true},
-    password: {type:String, required:true}
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true }
 })
 
 export class UserRepository {
 
     // This is a static method, so it can be called without creating an instance of the class
-    static async create({username, password}) {
-        
-        //1. username validaton
-        if (typeof username !== 'string') {
-            throw new Error('Username must be a string');
-        }
+    static async create({ username, password }) {
 
-        if (username.length < 3) {
-            throw new Error('Username must be at least 3 characters long');
-        }
+        //1. username validaton
+        Validation.username(username);
 
         //2. password validation
-        if (typeof password !== 'string' ) {
-            throw new Error('Password must be a string');
-        }
+        Validation.password(password);
 
-        if (password.length < 3) {
-            throw new Error('Password must be at least 3 characters long');
-        }
 
         //3. Make sure it does not exist in the db
-        const user = User.findOne({username});
+        const user = User.findOne({ username });
         if (user) throw new Error('User already exists');
 
         //4. Hash password
@@ -54,5 +43,48 @@ export class UserRepository {
         }).save()
 
         return id;
+    }
+
+    static async login ({ username, password }) {
+        //1. username validaton
+        Validation.username(username);
+
+        //2. password validation
+        Validation.password(password);
+
+        //3. Find user
+        const user = User.findOne({username});
+        if (!user) throw new Error('User does not exist')
+
+        //4. Compare password
+        const isValid = await bcrypt.compare(password, user.password);        
+        if (!isValid) throw new Error('Invalid password');
+
+        //5.remove password from user object that is sent in the response
+        const {password: _, ...publicUser} = user
+
+        return publicUser
+    }
+}
+
+class Validation {
+    static username(username) {
+        if (typeof username !== 'string') {
+            throw new Error('Username must be a string');
+        }
+
+        if (username.length < 3) {
+            throw new Error('Username must be at least 3 characters long');
+        }
+    }
+
+    static password(password) {
+        if (typeof password !== 'string') {
+            throw new Error('Password must be a string');
+        }
+
+        if (password.length < 3) {
+            throw new Error('Password must be at least 3 characters long');
+        }
     }
 }
